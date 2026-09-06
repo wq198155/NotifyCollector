351efb62a20e0b8ea6aa06e41ec1ab6b77fb7c15
smartWQ <smartwq@example.com>
2026-09-06 19:27:04 +0800

    chore: release v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.notifycollector"
         minSdk = 24
         targetSdk = 34
-        versionCode = 14
-        versionName = "1.14"
+        versionCode = 15
+        versionName = "1.1"
     }
 
     buildFeatures {
